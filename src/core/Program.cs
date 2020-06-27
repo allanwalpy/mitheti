f6c2fe2b49ccbe197288be3bdceea988359e0e61
allan_walpy <allanwalpy@gmail.com>
2020-06-27 22:26:03 +0300

    add hello world program.cs file

---
+++ src/core/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Mitheti.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
