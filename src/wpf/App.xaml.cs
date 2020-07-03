d29a04d0194435079d4a0593ba020ec2f203320a
allan_walpy <allanwalpy@gmail.com>
2020-07-03 21:53:50 +0300

    dump changes

--- src/wpf/App.xaml.cs
+++ src/wpf/App.xaml.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
+
+using Forms = System.Windows.Forms;
 
 namespace Mitheti.Wpf
 {
@@ -13,5 +11,55 @@ namespace Mitheti.Wpf
     /// </summary>
     public partial class App : Application
     {
+        private bool _isExiting = false;
+        private Forms.NotifyIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Closing += CloseWindow;
+
+            _notifyIcon = new Forms.NotifyIcon();
+            _notifyIcon.DoubleClick += (sender, args) => ShowWindow();
+            _notifyIcon.Icon = new System.Drawing.Icon("./Resources/trayIcon.png");
+            _notifyIcon.Visible = true;
+
+            this.CreateNotifyContextMenu();
+        }
+
+        private void CreateNotifyContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("Show").Click += (sender, args) => ShowWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (sender, args) => ExitApp();
+        }
+
+        private void ShowWindow()
+        {
+            MainWindow.Activate();
+            MainWindow.Show();
+        }
+
+        private void ExitApp()
+        {
+            _isExiting = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void CloseWindow(object sender, CancelEventArgs e)
+        {
+            if (_isExiting)
+            {
+                return;
+            }
+
+            e.Cancel = true;
+            MainWindow.Hide();
+        }
+
     }
 }
