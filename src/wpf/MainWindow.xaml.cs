01f1f79f858022f5ec1360bf937e6e312d08585d
allan_walpy <allanwalpy@gmail.com>
2020-07-03 15:59:15 +0300

    change wpf to Mitheti.Wpf namespace

--- src/wpf/MainWindow.xaml.cs
+++ src/wpf/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace wpf
+namespace Mitheti.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
