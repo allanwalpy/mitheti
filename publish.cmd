7f3b38467d410be753c5a566337b7df78548c86b
allan_walpy <allanwalpy@gmail.com>
2020-07-07 18:56:06 +0300

    some tmp fix to publish script

--- publish.cmd
+++ publish.cmd
@@ -1,8 +1,11 @@
+:? builds projects as self contained;
+
 dotnet publish ^
     --output ./out/publish ^
     --configuration Release ^
     --runtime win-x64 ^
     --self-contained true ^
-    -p:PublishSingleFile=true ^
     -p:PublishTrimmed=true ^
     -p:PublishReadyToRun=true
+
+:TODO: figure out why `-p:PublishSingleFile=true` fails on web project;
