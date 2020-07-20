8abf469c999b6e204bd966f1b2a6aa6505e1ed37
allan_walpy <allanwalpy@gmail.com>
2020-07-20 10:53:55 +0300

    #6 add todo comment;

--- src/core/Services/ISizeLimitDatabaseService.cs
+++ src/core/Services/ISizeLimitDatabaseService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Mitheti.Core.Services
 {
+    // TODO: add servicing service, with this and other (optimizing and clear records) services on tasks;
     public interface ISizeLimitDatabaseService
     {
         void LimitDatabase();
